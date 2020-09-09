45132d36595d1059f8ba389996153083cb184a56
Xavier Cho <mysticfallband@gmail.com>
2020-09-09 18:12:09 +0900

    Add basic test case for Context

---
+++ tests/test_context.py
@@ -0,0 +1,30 @@
+import unittest
+from typing import Sequence
+
+from cairo import ImageSurface, Format
+
+from alleycat.ui import Context, Input, FakeMouseInput
+from alleycat.ui.cairo import CairoToolkit, CairoContext
+
+
+class ContextTest(unittest.TestCase):
+    def test_inputs(self):
+        class TestMouseInput(FakeMouseInput):
+            pass
+
+        class TestToolkit(CairoToolkit):
+
+            def create_inputs(self, ctx: Context) -> Sequence[Input]:
+                return TestMouseInput(ctx),
+
+        toolkit = TestToolkit()
+        context = CairoContext(toolkit, ImageSurface(Format.ARGB32, 10, 10))
+
+        mouse_input = context.mouse_input
+
+        self.assertIsNotNone(mouse_input)
+        self.assertIs(TestMouseInput, type(mouse_input))
+
+
+if __name__ == '__main__':
+    unittest.main()
