755a789ae9fd9dddd3d4f9756cdc836e8aed2aeb
Xavier Cho <mysticfallband@gmail.com>
2020-10-02 12:48:45 +0900

    Code clean up: use UI context created in setUp()

--- tests/test_label.py
+++ tests/test_label.py
@@ -1,7 +1,6 @@
 import unittest
 
 from alleycat.ui import Label, Bounds, Window, RGBA
-from alleycat.ui.cairo import UI
 from alleycat.ui.glass import ColorKeys
 from tests.ui import UITestCase
 
@@ -18,17 +17,15 @@ class LabelTest(UITestCase):
         self.assertEqual(["Label.background", "background"], keys)
 
     def test_draw(self):
-        context = UI().create_context()
-
-        window = Window(context)
+        window = Window(self.context)
         window.bounds = Bounds(0, 0, 100, 60)
 
-        label = Label(context)
+        label = Label(self.context)
 
         label.text = "Label"
         label.bounds = Bounds(10, 20, 60, 30)
 
-        label2 = Label(context)
+        label2 = Label(self.context)
 
         label2.text = "Test"
         label2.size = 20
@@ -38,9 +35,9 @@ class LabelTest(UITestCase):
         window.add(label)
         window.add(label2)
 
-        context.process()
+        self.context.process()
 
-        self.assertImage("draw", context)
+        self.assertImage("draw", self.context)
 
 
 if __name__ == '__main__':
