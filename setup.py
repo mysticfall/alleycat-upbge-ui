706708a3c815499ebbb718238632aa6fbe6618c1
Xavier Cho <mysticfallband@gmail.com>
2020-11-18 07:34:07 +0900

    Add setup.py for packaging and local testing

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="alleycat-ui",
+    version="0.0.1",
+    author="Xavier Cho",
+    author_email="mysticfallband@gmail.com",
+    description="A GUI widget toolkit for UPBGE (Uchronia Project Blender Game Engine).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mysticfall/alleycat-ui",
+    packages=find_packages(),
+    install_requires=["alleycat-reactive==0.4.3", "returns==0.15.0", "rx==3.1.1"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
