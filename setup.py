fc858110a755afdd2b8f9c76a7dbe30ec93bed55
Xavier Cho <mysticfallband@gmail.com>
2021-01-01 19:04:41 +0900

    Fix dependency problem regarding mypy
    This time, I really did!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="alleycat-ui",
-    version="0.0.2",
+    version="0.0.3",
     author="Xavier Cho",
     author_email="mysticfallband@gmail.com",
     description="Lightweight GUI widget toolkit for UPBGE (Uchronia Project Blender Game Engine).",
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/mysticfall/alleycat-ui",
     packages=find_packages(),
-    install_requires=["alleycat-reactive==0.4.3", "returns==0.15.0", "rx==3.1.1", "pangocairocffi==0.5.0"],
+    install_requires=["alleycat-reactive==0.4.5", "returns==0.15.0", "rx==3.1.1", "pangocairocffi==0.5.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
