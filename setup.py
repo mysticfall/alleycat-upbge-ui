51ea6eaece07c71736cab8fdbf1ac7a38e8288af
Xavier Cho <mysticfallband@gmail.com>
2021-01-10 17:31:01 +0900

    Fix setup tool settings concerning namespace package
    This time, for real... (and I HATE Python!)

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name="alleycat-ui",
-    version="0.0.4",
+    version="0.0.5",
     author="Xavier Cho",
     author_email="mysticfallband@gmail.com",
     description="Lightweight GUI widget toolkit for UPBGE (Uchronia Project Blender Game Engine).",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mysticfall/alleycat-ui",
-    packages=find_packages(),
-    install_requires=["alleycat-reactive==0.4.6", "returns==0.15.0", "rx==3.1.1", "pangocairocffi==0.5.0"],
+    packages=find_namespace_packages(include=["alleycat.*"]),
+    install_requires=["alleycat-reactive==0.4.7", "returns==0.15.0", "rx==3.1.1", "pangocairocffi==0.5.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
